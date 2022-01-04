53bd0bce8e3ef1eba4cdde98c943a3eafe575c3d
Amubasshir <lastmubasshir@gmail.com>
2022-01-04 19:09:54 +0600

    image and data added

---
+++ public/blogData.js
@@ -0,0 +1,87 @@
+export const blogList = [
+  {
+    id: 1,
+    title: 'The ultimate benefit of Running ',
+    category: 'health',
+    subCategory: ['active', 'fit', 'daily'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'Ruhul Amin',
+    authorAvatar: '/assets/images/blogImages/a1.png',
+    createdAt: 'December 03, 2021',
+    cover: '/assets/images/blogImages/b-i-1.png',
+  },
+  {
+    id: 2,
+    title: 'Back Pain- The Modern Terror',
+    category: 'health',
+    subCategory: ['fit', 'advice', 'modern-lifestyle'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'Sohel Rana',
+    authorAvatar: '/assets/images/blogImages/a2.png',
+    createdAt: 'November 09, 2021',
+    cover: '/assets/images/blogImages/b-i-2.png',
+  },
+  {
+    id: 3,
+    title: 'Yoga- The Anchient Art Of Well Being',
+    category: 'Yoga',
+    subCategory: ['anchient', 'life-style', 'fresh'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'Shuvro',
+    authorAvatar: '/assets/images/blogImages/a3.png',
+    createdAt: 'July 21, 2021',
+    cover: '/assets/images/blogImages/b-i-3.png',
+  },
+  {
+    id: 4,
+    title: 'Diabetes The Silent Killer',
+    category: 'health',
+    subCategory: ['daily', 'well-being', 'insulin'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'Mubasshir Ahmed',
+    authorAvatar: '/assets/images/blogImages/a4.png',
+    createdAt: 'June 17, 2021',
+    cover: '/assets/images/blogImages/b-i-4.png',
+  },
+  {
+    id: 5,
+    title: 'Modern Invention In Health',
+    category: 'invention',
+    subCategory: ['modern', 'daily'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'Ruhul Amin',
+    authorAvatar: '/assets/images/blogImages/a1.png',
+    createdAt: 'March 29, 2021',
+    cover: '/assets/images/blogImages/b-i-5.png',
+  },
+  {
+    id: 6,
+    title: 'Pregnancy- Things To Know',
+    category: 'women health',
+    subCategory: ['women', 'child', 'time'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'Sohel Rana',
+    authorAvatar: '/assets/images/blogImages/a2.png',
+    createdAt: 'January 19, 2021',
+    cover:
+      '/assets/images/blogImages/b-i-6.png',
+  },
+  {
+    id: 7,
+    title: 'Corona- The Ultimate Scare',
+    category: 'new',
+    subCategory: ['virus', 'china', 'covid'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.sum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    authorName: 'Shuvro',
+    authorAvatar: '/assets/images/blogImages/a3.png',
+    createdAt: 'December 16, 2020',
+    cover: '/assets/images/blogImages/b-i-7.png',
+  },
+];
